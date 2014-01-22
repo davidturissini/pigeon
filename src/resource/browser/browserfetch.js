4fc3ce264e6a251380f64a05527d43edbb0939cb
Dave Turissini <davidairborne@gmail.com>
2014-01-21 22:59:06 -0800

    Fixing incorrect method syntax on browser fetch

--- src/resource/browser/browserfetch.js
+++ src/resource/browser/browserfetch.js
@@ -1,20 +1,11 @@
 var ajax = require('component-ajax');
 var Q = require('q');
 
-
-function browserFetch (path, params) {
-
-	return ajax(path, {
-		dataType:'text',
-		data:params || {}
-	});
-}
-
-
 exports.fetch = function (path, params) {
 	var defer = Q.defer();
 
-	ajax(path, {
+	ajax({
+		url:path,
 		dataType:'text',
 		data:params || {},
 		success: function (e) {
