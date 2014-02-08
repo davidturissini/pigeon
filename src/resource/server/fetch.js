dcc08cbb3985832ddaf462e320b39c70b31abbc1
Dave Turissini <davidairborne@gmail.com>
2014-02-07 20:28:05 -0800

    Fixing issue where params object wasn't being passed properly

--- src/resource/server/fetch.js
+++ src/resource/server/fetch.js
@@ -1,10 +1,10 @@
 var fsfetch = require('./fsfetch').fetch;
 var urlfetch = require('./urlfetch').fetch;
 
-exports.fetch = function resourceFetch (path) {
+exports.fetch = function resourceFetch (path, params) {
 	
 	if (/http\:\/\//.test(path)) {
-		return urlfetch(path);
+		return urlfetch(path, params);
 	} else {
 
 		return fsfetch(path);
