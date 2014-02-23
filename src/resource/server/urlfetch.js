7c69f490e8fbe0bc1450fda2f4e2fe0d03beb6b8
Dave Turissini <davidairborne@gmail.com>
2014-02-22 21:04:27 -0800

    Lowering maxSockets

--- src/resource/server/urlfetch.js
+++ src/resource/server/urlfetch.js
@@ -1,7 +1,7 @@
 var Q = require('q');
 var request = require('request');
 var querystring = require('querystring');
-require('http').globalAgent.maxSockets = 1024;
+require('http').globalAgent.maxSockets = 200;
 
 function fetch (path, params) {
 	params = params || {};
@@ -13,13 +13,17 @@ function fetch (path, params) {
 	request({
 
 		url:url,
-
 		headers:{
 			'Connection':'keep-alive'
+		},
+		pool:{
+			maxSockets:200
 		}
 
 	}, function (err, data) {
 		if (err) {
+			console.log(path);
+			console.log(err.stack);
 			defer.reject(err);
 		} else {
 			defer.resolve(data.body, data);
